9d14fb64beb2af8cd76b57226817dc06a14e5f61
wh0820Android <32416380+wh0820Android@users.noreply.github.com>
2018-02-05 11:04:16 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+// Our closure
+class Test{
+    public  function quoteValue($value){
+        trigger_error(
+            'Attempting to quote a value in ' . get_class($this) .
+            ' without extension/driver support can introduce security vulnerabilities in a production environment'
+        );
+        return '\'' . addcslashes((string) $value, "\x00\n\r\\'\"\x1a") . '\'';
+
+    }
+    public  function quoteValueList(){
+        $valueList = array(
+            1 => 'wh',
+            2 => 'wh2',
+            3 => array(
+                1 => 12,
+                2 => 13
+            )
+        );
+
+        return implode(', ', array_map([$this, 'quoteValue'], (array) $valueList));
+    }
+}
+
+$test = new Test();
+echo $test->quoteValueList();
+
+?>
